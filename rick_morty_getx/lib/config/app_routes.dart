74448124ad2c4b5a9c216961910e33c8aa4e133f
Daniel <danielccubillos@hotmail.com>
2025-03-01 04:34:32 -0500

    Update: App routes updated with characterBinding

--- rick_morty_getx/lib/config/app_routes.dart
+++ rick_morty_getx/lib/config/app_routes.dart
@@ -1,16 +1,21 @@
 import 'package:get/get.dart';
+import '../presentation/bindings/character_binding.dart';
 import '../presentation/pages/welcome_page.dart';
 import '../presentation/pages/character_list.dart';
 import '../presentation/pages/character_info.dart';
 
 class AppRoutes {
-  static const String welcome = '/welcome';
-  static const String characterList = '/character-list';
-  static const String characterInfo = '/character-info';
+  static const welcome = '/welcome';
+  static const characterList = '/character-list';
+  static const characterInfo = '/character-info';
 
   static final routes = [
     GetPage(name: welcome, page: () => WelcomePage()),
-    GetPage(name: characterList, page: () => CharacterListPage()),
+    GetPage(
+      name: characterList,
+      page: () => const CharacterListPage(),
+      binding: CharacterBinding(),
+    ),
     GetPage(name: characterInfo, page: () => const CharacterInfoPage()),
   ];
 }
